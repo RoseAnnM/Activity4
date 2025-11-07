ce076fff251bba74c5287d28b3b7d2d629d8267b
24-36772-Maranan <24-36772@g.batstate-u.edu.ph>
2025-11-07 22:32:21 +0800

    Update veterinary.sql

--- veterinary.sql
+++ veterinary.sql
@@ -145,3 +145,7 @@ ALTER TABLE invoices RENAME COLUMN paymentdate TO paymenttime;
 
 DELETE FROM appointments 
 WHERE animalid = (SELECT animalid FROM animals WHERE name = 'Simba');
+
+UPDATE doctors 
+SET dlastname = 'Reyes-Gonzales' 
+WHERE dfirstname = 'Dr. Sofia' AND dlastname = 'Reyes';
