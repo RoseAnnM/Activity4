f260f853f4cae5faf34e2ba3b95eaaaec2bab2c7
RoseAnnM <24-32751@g.batstate-u.edu.ph>
2025-11-07 22:30:17 +0800

    Update veterinary.sql

--- veterinary.sql
+++ veterinary.sql
@@ -142,3 +142,6 @@ VALUES
 ALTER TABLE owners ADD COLUMN registereddate DATE;
 
 ALTER TABLE invoices RENAME COLUMN paymentdate TO paymenttime;
+
+DELETE FROM appointments 
+WHERE animalid = (SELECT animalid FROM animals WHERE name = 'Simba');
